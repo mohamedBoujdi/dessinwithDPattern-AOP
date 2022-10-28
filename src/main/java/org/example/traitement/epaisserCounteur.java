cfd3be46b787db6a116e76e44df72351d1bd130f
BOUJDI Mohamed <boujdi.medz@gmail.com>
2022-10-28 15:05:40 +0100

    feat:traite epaisseur de counteur strategy

---
+++ src/main/java/org/example/traitement/epaisserCounteur.java
@@ -0,0 +1,14 @@
+package org.example.traitement;
+
+import org.example.figures.Figure;
+import org.example.parametres.Parameterage;
+
+public class epaisserCounteur implements TraitementStrategy {
+    @Override
+    public void traiter(Figure figure) {
+        //traiter epaisseur les figures
+        Parameterage parameterage = new Parameterage();
+        parameterage.setEpaisseurContour(figure.getParameterage().getEpaisseurContour()+ 1);
+        figure.setParameterage(parameterage);
+    }
+}
